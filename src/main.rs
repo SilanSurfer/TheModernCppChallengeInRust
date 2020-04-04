5122e556ca01c3982d39de2a39c3ef4292c64861
Aszwarc <szwarc.adam@gmail.com>
2020-04-04 20:30:03 +0200

    feature: 1.1 Sum of naturals divisible by 3 and 5
    Implementation of function for calculating sum of natural numbers
    divisible by 3 and 5 up to range given by user.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
+fn sum_of_naturals_div_by_3_and_5() {
+    println!("This function will calculate the sum of numbers up
+    to limit that are divisible by 3 and 5");
+    let mut input_buffer = String::new();
+    io::stdin().read_line(&mut input_buffer).expect("Failed to read input line");
+    let upper_limit = input_buffer.trim().parse::<u64>().unwrap();
+
+    let mut sum = 0;
+    for no in 1..upper_limit {
+        if no % 3 == 0 && no % 5 == 0 {
+            sum += no;
+        }
+    }
+    println!("Sum is {}", sum);
+}
+
 fn main() {
-    println!("Hello, world!");
+    sum_of_naturals_div_by_3_and_5();
 }
