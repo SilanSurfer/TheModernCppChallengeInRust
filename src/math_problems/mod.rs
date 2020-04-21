de7e1361b4077efa50ffd14c03affab4e7233f79
Aszwarc <szwarc.adam@gmail.com>
2020-04-21 19:56:50 +0200

    refactoring: Removed not needed prints in functions

--- src/math_problems/mod.rs
+++ src/math_problems/mod.rs
@@ -1,8 +1,6 @@
 use std::io;
 
 pub fn sum_of_naturals_div_by_3_and_5() {
-    println!("This function will calculate the sum of numbers up
-    to limit that are divisible by 3 and 5");
     let mut input_buffer = String::new();
     io::stdin().read_line(&mut input_buffer).expect("Failed to read input line");
     let upper_limit = input_buffer.trim().parse::<u64>().unwrap();
@@ -17,7 +15,6 @@ pub fn sum_of_naturals_div_by_3_and_5() {
 }
 
 pub fn greatest_common_divisor() {
-    println!("This function will calculate greatest common divisor of two given numbers");
     let mut input_buffer = String::new();
     io::stdin().read_line(&mut input_buffer).expect("Failed to read input line");
     let mut buff_iter = input_buffer.split_whitespace();
